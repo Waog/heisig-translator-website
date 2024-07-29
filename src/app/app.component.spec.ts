1b49fb446b4a383b7a990fe92a644ce2a4a43fde
Waog <oliver.stadie@gmail.com>
2024-07-30 01:56:11 +0200

    ADD some tests for app component
    - even though they are not ideal

---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,66 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { InputTextComponent } from './input-text/input-text.component';
+import { AudioService } from './play-audio/audio.service';
+import { SentenceTranslationComponent } from './sentence-translation/sentence-translation.component';
+import { TranslationService } from './sentence-translation/translation.service';
+import { PinyinService } from './single-char-translation/pinyin.service';
+import { SingleCharTranslationComponent } from './single-char-translation/single-char-translation.component';
+
+describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [AppComponent, HttpClientTestingModule],
+      providers: [TranslationService, PinyinService, AudioService],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create the app', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should update userInput when input changes', () => {
+    const inputTextComponent = fixture.debugElement.query(
+      By.directive(InputTextComponent)
+    );
+    inputTextComponent.componentInstance.userInputChange.emit('Hello');
+
+    expect(component.userInput).toBe('Hello');
+  });
+
+  it('should pass userInput to sentence translation components', () => {
+    component.userInput = 'Hello';
+    fixture.detectChanges();
+
+    const sentenceTranslationComponents = fixture.debugElement.queryAll(
+      By.directive(SentenceTranslationComponent)
+    );
+    expect(sentenceTranslationComponents[0].componentInstance.userInput).toBe(
+      'Hello'
+    );
+    expect(sentenceTranslationComponents[1].componentInstance.userInput).toBe(
+      'Hello'
+    );
+  });
+
+  it('should pass userInput to single char translation component', () => {
+    component.userInput = 'Hello';
+    fixture.detectChanges();
+
+    const singleCharTranslationComponent = fixture.debugElement.query(
+      By.directive(SingleCharTranslationComponent)
+    );
+    expect(singleCharTranslationComponent.componentInstance.userInput).toBe(
+      'Hello'
+    );
+  });
+});
