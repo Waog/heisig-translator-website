2793fed0a9848ad6427b3f989737361c4ee685f5
Waog <oliver.stadie@gmail.com>
2024-07-29 15:26:58 +0200

    ADD simple heisig translation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,31 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { mapping } from './mapping';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'heisig-translator-website';
+  userInput: string = '';
+  mapping = mapping;
+
+  get modifiedInput(): string {
+    return this.transformInput(this.userInput);
+  }
+
+  transformInput(input: string): string {
+    let result = input;
+    for (const key in this.mapping) {
+      if (this.mapping.hasOwnProperty(key)) {
+        const regex = new RegExp(key, 'g');
+        result = result.replace(regex, this.mapping[key] + `, `);
+      }
+    }
+    return result;
+  }
 }
---
+++ src/app/mapping.ts
@@ -0,0 +1,4 @@
+export const mapping: { [key: string]: string } = {
+  你: 'you',
+  好: 'good',
+};
