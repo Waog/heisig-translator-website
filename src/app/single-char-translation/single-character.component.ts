44e879b358fa9a80057b5010a06ffe7c309fa8c5
Waog <oliver.stadie@gmail.com>
2024-07-30 16:48:36 +0200

    UPDATE #1 Use up less space for non-chinese characters

--- src/app/single-char-translation/single-character.component.ts
+++ src/app/single-char-translation/single-character.component.ts
@@ -1,11 +1,5 @@
 import { CommonModule } from '@angular/common';
-import {
-  Component,
-  Input,
-  OnChanges,
-  OnInit,
-  SimpleChanges,
-} from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { pinyin } from 'pinyin-pro';
 import { mapping } from '../mapping';
 
@@ -16,20 +10,22 @@ import { mapping } from '../mapping';
   templateUrl: './single-character.component.html',
   styleUrls: ['./single-character.component.scss'],
 })
-export class SingleCharacterComponent implements OnChanges, OnInit {
+export class SingleCharacterComponent implements OnChanges {
   @Input() hanzi: string = '';
   pinyin: string = '';
   heisig: string = '';
-
-  ngOnInit(): void {
-    this.pinyin = pinyin(this.hanzi);
-    this.heisig = mapping[this.hanzi] || this.hanzi;
-  }
+  isChinese: boolean = false;
 
   ngOnChanges(changes: SimpleChanges): void {
     if (changes['hanzi']) {
       this.pinyin = pinyin(this.hanzi);
       this.heisig = mapping[this.hanzi] || this.hanzi;
+      this.isChinese = this.isChineseCharacter(this.hanzi);
     }
+  }
+
+  isChineseCharacter(char: string): boolean {
+    const chineseCharacterRegex = /[\u4e00-\u9fff]/;
+    return chineseCharacterRegex.test(char);
   }
 }
