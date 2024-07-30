07732cf0a1f868d04614ade8ca5cc28ee8a67646
Waog <oliver.stadie@gmail.com>
2024-07-30 11:33:30 +0200

    ADD #5 PWA for share capabilities

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,45 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { appConfig } from './app/app.config';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, appConfig).catch((err) =>
+  console.error(err)
+);
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('/service-worker.js')
+      .then((registration) => {
+        console.log(
+          'Service Worker registered with scope:',
+          registration.scope
+        );
+
+        registration.onupdatefound = () => {
+          const installingWorker = registration.installing;
+          if (installingWorker) {
+            installingWorker.onstatechange = () => {
+              if (installingWorker.state === 'installed') {
+                if (navigator.serviceWorker.controller) {
+                  // Neue Version verfügbar
+                  console.log('Neue Version verfügbar. Jetzt aktualisieren?');
+                  if (confirm('Neue Version verfügbar. Jetzt aktualisieren?')) {
+                    window.location.reload();
+                  }
+                }
+              }
+            };
+          }
+        };
+      })
+      .catch((error) => {
+        console.log('Service Worker registration failed:', error);
+      });
+
+    // Überprüfen, ob der Service Worker aktiviert und installiert ist
+    navigator.serviceWorker.ready.then((registration) => {
+      registration.update();
+    });
+  });
+}
---
+++ src/service-worker.js
@@ -0,0 +1,21 @@
+self.addEventListener("install", (event) => {
+  console.log("Service Worker installing.");
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  console.log("Service Worker activating.");
+  event.waitUntil(clients.claim());
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        return response;
+      })
+      .catch(() => {
+        return caches.match(event.request);
+      })
+  );
+});
