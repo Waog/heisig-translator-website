f5674c20cd6b40c3096f48a82058f10afb234664
waog <hello@waog.net>
2024-07-30 09:34:11 +0000

    Auto-generated commit
    Triggered by commit: https://github.com/Waog/heisig-translator-website/commit/9512dadf11c90d6f6dcc66dcb0c5465fe9295a1d

---
+++ service-worker.js
@@ -0,0 +1,11 @@
+self.addEventListener("install", (event) => {
+  console.log("Service Worker installing.");
+});
+
+self.addEventListener("activate", (event) => {
+  console.log("Service Worker activating.");
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(fetch(event.request));
+});
